d9acfe611749531d98d2c6c568806d40617f9816
lan wang <wangyiming-1314@qq.com>
2026-09-19 16:08:37 +0800

    feat: initialize Studypilot spring Boot project

---
+++ src/main/java/com/wym/studypilot/StudypilotApplication.java
@@ -0,0 +1,13 @@
+package com.wym.studypilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudypilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudypilotApplication.class, args);
+	}
+
+}
